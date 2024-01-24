5a067238c3b98a307ef043a90abf1659f9e4270a
Stefanie Plieschnegger <stefanie.plieschnegger@localstack.cloud>
2024-01-24 18:15:33 +0100

    wip: test for rds

--- __tests__/resolvers.test.js
+++ __tests__/resolvers.test.js
@@ -59,3 +59,70 @@ describe("dynamodb resolvers", () => {
     await checkResolverValid(code, responseContext, "response");
   });
 });
+
+describe("rds resolvers", () => {
+  test("postgres", async () => {
+    const code = `
+    import { select, createPgStatement, toJsonObject, typeHint } from "@aws-appsync/utils/rds"
+    // appsync/resolvers/Query.getPracticeById.js
+    export function request(ctx) {
+
+        //const whereClause = { id: { eq: typeHint.UUID(ctx.args.id) } }; // TODO ctx.args.id is undefined 
+        const whereClause = { id: { eq: typeHint.UUID("123") } };
+        return createPgStatement(select({
+            table: "UserGroup",
+            where: whereClause
+        }));
+    }
+
+    export function response(ctx) {
+        const { error, result } = ctx;
+        if (error) {
+            return util.appendError(error.message, error.type, result);
+        }
+        const items = toJsonObject(result)[0];
+        if (items.length == 0) {
+            return null;
+        }
+        return items[0];
+    }
+    `;
+
+    const requestContext = {
+      args: {
+        id: "1232"
+      }
+    };
+
+    await checkResolverValid(code, requestContext, "request");
+
+    const test_result = {
+      sqlStatementResults: [
+        {
+          numberOfRecordsUpdated: 0,
+          records: [
+            [
+              {
+                stringValue: "User One"
+              },
+              {
+                stringValue: "1232"
+              }
+            ],
+          ],
+          columnMetadata: [
+            
+          ]
+        }
+      ]
+    }
+
+    const responseContext = {
+      error: null,
+      result: JSON.stringify(test_result) // TODO the result is not correctly formatted? :/
+    };
+
+    await checkResolverValid(code, responseContext, "response");
+  });
+});
+
