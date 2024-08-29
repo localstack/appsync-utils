f00aeb82088509fff8997974cbc343bc811b5f97
Dzmitry Yeliseyeu <dzmitry_yeliseyeu1@epam.com>
2024-08-29 17:49:28 +0300

    tests and fix

--- rds/index.js
+++ rds/index.js
@@ -10,28 +10,27 @@ export function toJsonObject(inputStr) {
   for (const { records, columnMetadata } of input.sqlStatementResults) {
     const statement = [];
     if(typeof records === 'undefined' ){
-      perStatement.push({});
-      continue;
-    }
-    for (const record of records) {
-      const row = {};
-      if (record.length !== columnMetadata.length) {
-        // TODO: what to do here?!
-        throw new Error("TODO");
+      statement.push({});
+    } else {
+      for (const record of records) {
+        const row = {};
+        if (record.length !== columnMetadata.length) {
+          // TODO: what to do here?!
+          throw new Error("TODO");
+        }
+
+        for (const colNo in record) {
+
+          // TODO: what if the column is not a string?
+          const { stringValue } = record[colNo];
+          const { label } = columnMetadata[colNo];
+
+          row[label] = stringValue;
+        }
+
+        statement.push(row);
       }
-
-      for (const colNo in record) {
-
-        // TODO: what if the column is not a string?
-        const { stringValue } = record[colNo];
-        const { label } = columnMetadata[colNo];
-
-        row[label] = stringValue;
-      }
-
-      statement.push(row);
     }
-
     perStatement.push(statement);
   }
   return perStatement;
@@ -314,37 +313,37 @@ export function createMySQLStatement(...statements) {
 
 
 export const typeHint = {
-  DECIMAL: function(value) {
+  DECIMAL: function (value) {
     return {
       type: "DECIMAL",
       value,
     };
   },
-  JSON: function(value) {
+  JSON: function (value) {
     return {
       type: "JSON",
       value,
     };
   },
-  TIME: function(value) {
+  TIME: function (value) {
     return {
       type: "TIME",
       value,
     };
   },
-  DATE: function(value) {
+  DATE: function (value) {
     return {
       type: "DATE",
       value,
     };
   },
-  UUID: function(value) {
+  UUID: function (value) {
     return {
       type: "UUID",
       value,
     };
   },
-  TIMESTAMP: function(value) {
+  TIMESTAMP: function (value) {
     return {
       type: "TIMESTAMP",
       value: value.toISOString(),
