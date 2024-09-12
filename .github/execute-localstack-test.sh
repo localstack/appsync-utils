fe7905148d3b4d2bd10f3a53f78f337248805ad1
Simon Walker <s.r.walker101@googlemail.com>
2024-09-12 13:45:57 +0100

    Move localstack test to an external file

---
+++ .github/execute-localstack-test.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+log() {
+    echo $@ >&2
+}
+
+(cd cdk
+
+set -x
+
+log "Installing dependencies"
+npm ci
+
+log "Bootstrapping"
+npm run cdklocal -- bootstrap
+
+log "Deploying"
+npm run cdklocal -- deploy --require-approval never --outputs-file outputs.json
+
+export TEST_URL=$(jq -r .CdkStack.GraphQLURL outputs.json)
+export TEST_API_KEY=$(jq -r .CdkStack.ApiKey outputs.json)
+
+log "Accessing URL ${TEST_URL} with api key ${TEST_API_KEY}"
+curl \
+    --connect-timeout 30 \
+    --retry 10 \
+    --retry-delay 6 \
+    -X POST \
+    -H "x-api-key:$TEST_API_KEY" \
+    -H "Accept:application/json" \
+    -H "Content-Type:application/json" \
+    $TEST_URL \
+    -d '{"query": "query { foo }"}'
+
+)
